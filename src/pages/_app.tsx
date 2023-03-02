12041425845b08077b3a2b2805b2041e8a9dea4c
Luigi Oliveira <luigijordanio@hotmail.com>
2023-03-02 09:14:58 -0300

    Resolve lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,11 @@
 import { PrismicProvider } from '@prismicio/react';
 import { AppProps } from 'next/app';
 import Link from 'next/link';
-import { linkResolver } from '../services/prismic';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <PrismicProvider
-      linkResolver={linkResolver}
       internalLinkComponent={({ href, ...props }) => (
         <Link href={href}>
           <a {...props} />
